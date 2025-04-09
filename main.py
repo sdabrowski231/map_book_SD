594cadade1ca7a7d10a6d38674e1d2606d7c39d7
sdabrowski231 <u64721@student.wat.edu.pl>
2025-04-09 17:31:19 +0200

    chore: Add menu

--- main.py
+++ main.py
@@ -4,8 +4,19 @@ from utils.kontroller import get_user_info
 
 def main():
     get_user_info(users)
+    print(f"Witaj {users[0]["name"]}")
+
+    while True:
+        print("=====menu=====")
+        print("0 - zakończ program")
+        print("1 - pokaż co u znajomych ")
+        print("==============")
+        choice=input("wybierz opcje MENU: ")
+        if choice=="0": break
+        if choice=="1": get_user_info(users)
 
 
 if __name__ == "__main__":
     main()
+
 
