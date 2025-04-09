0e6f52f6bcdf78b6cb819a4be0e017e1aa9e1ad9
sdabrowski231 <u64721@student.wat.edu.pl>
2025-04-09 17:17:47 +0200

    chore: Simple data structure improve

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-users:list=[
-    {"name":"Szymon","location":"Złocieniec","posts":3},
-    {"name":"Oliwier","location":"Zamość","posts":4},
-    {"name":"Kuba","location":"Warszawa","posts":320},
-    {"name":"Konrad","location":"Lublin","posts":12},
-]
-def get_user_info(users_data:list)->None:
-    for user in users_data:
-        print(f"twój znajomy {user["name"]} z miejscowości {user["location"]} opublikował {user['posts']} postów ")
-get_user_info(users)
+from utils.model import users
+from utils.kontroller import get_user_info
+
+
+def main():
+    get_user_info(users)
+
+
+if __name__ == "__main__":
+    main()
+
