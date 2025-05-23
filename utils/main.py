eef5c80e1d4097ea90c56d39faf5e0e01910641e
sdabrowski231 <u64721@student.wat.edu.pl>
2025-05-23 20:59:08 +0200

    chore: the code was restructed according to the mvc template

---
+++ utils/main.py
@@ -0,0 +1,10 @@
+from utils.model import users
+from utils.kontroller import get_user_info, add_user, remove_user,edit_user:
+
+
+
+ def main():
+     get_user_info(users)
+
+if __name__ == '__main__':
+    main()
---
+++ utils/model.py
@@ -0,0 +1,6 @@
+users:list=[
+    {"name":"Szymon","location":"Złocieniec","posts":3},
+    {"name":"Oliwier","location":"Zamość","posts":4},
+    {"name":"Kuba","location":"Warszawa","posts":320},
+    {"name":"Konrad","location":"Lublin","posts":12},
+]
