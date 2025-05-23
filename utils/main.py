6e66b7915b74ae13a24703b0ebe0d01f0e314a0e
sdabrowski231 <u64721@student.wat.edu.pl>
2025-05-23 21:08:39 +0200

    wip: add menu

--- utils/main.py
+++ utils/main.py
@@ -2,9 +2,17 @@ from utils.model import users
 from utils.kontroller import get_user_info, add_user, remove_user,edit_user:
 
 
+def main():
+    get_user_info(users)
+    print(f"Witaj {users[0]["name"]}")
+    while True:
+        print("=====menu=====")
+        print("0 - zakończ program")
+        print("1 - pokaż co u znajomych ")
+            choice = input("wybierz opcje MENU: ")
+            if choice == "0": break
+            if choice == "1": get_user_info(users)
 
- def main():
-     get_user_info(users)
 
 if __name__ == '__main__':
     main()
